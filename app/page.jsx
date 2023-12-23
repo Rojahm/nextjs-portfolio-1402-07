549f383f2f6137e3d49466b6f0fbea691b5a1ccd
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-12-23 12:35:09 +0330

    dark mode start

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,9 @@ import Slide from "./components/slide/Slide";
 export default function Home() {
   return (
     <main className="min-w-full flex flex-col h-screen overflow-y-scroll snap-y snap-mandatory scroll-smooth">
-      <Hero />
+      <div className="h-screen snap-start flex justify-center items-center min-h-screen bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-rose-100 from-0% to-rose-50 to-25%">
+        <Hero />
+      </div>
       <div className="min-h-screen h-screen snap-start">
         <Slide />
       </div>
